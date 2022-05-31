6b94d322769a142472ac8375936b90dba6dcbf7a
bertrand <bertrand.pautet@cpe.fr>
2022-05-31 14:27:50 +0200

    add serving static + nginx

---
+++ userService/src/main/java/com/asi/dto/UserDto.java
@@ -0,0 +1,47 @@
+package com.asi.dto;
+
+public class UserDto {
+	private int idUser;
+	private String email;
+	private String name;
+	private String surname;
+	private String token;
+	private double money;
+	
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public double getMoney() {
+		return money;
+	}
+	public void setMoney(double money) {
+		this.money = money;
+	}
+}
---
+++ userService/src/main/java/com/asi/model/User.java
@@ -0,0 +1,98 @@
+package com.asi.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue
+	@Column
+	private int idUser;
+	@Column
+	private String nameUser;
+	@Column
+	private String surnameUser;
+	@Column(unique=true)
+	private String emailUser;
+	@Column
+	private double moneyUser;
+	@Column
+	private String passwordUser;
+	
+	public User() {
+	}
+	
+	public User(int idUser, String nameUser, String surnameUser, String emailUser, double moneyUser, String passwordUser) {
+		super();
+		this.idUser = idUser;
+		this.nameUser = nameUser;
+		this.surnameUser = surnameUser;
+		this.emailUser = emailUser;
+		this.moneyUser = moneyUser;
+		this.passwordUser = passwordUser;
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNameUser() {
+		return nameUser;
+	}
+
+	public void setNameUser(String nameUser) {
+		this.nameUser = nameUser;
+	}
+
+	public String getSurnameUser() {
+		return surnameUser;
+	}
+
+	public void setSurnameUser(String surnameUser) {
+		this.surnameUser = surnameUser;
+	}
+	
+	public String getEmailUser() {
+		return emailUser;
+	}
+
+	public void setEmailUser(String emailUser) {
+		this.emailUser = emailUser;
+	}
+
+	public double getMoneyUser() {
+		return moneyUser;
+	}
+
+	public void setMoneyUser(double moneyUser) {
+		this.moneyUser = moneyUser;
+	}
+
+	public String getPasswordUser() {
+		return passwordUser;
+	}
+
+	public void setPasswordUser(String passwordUser) {
+		this.passwordUser = passwordUser;
+	}
+	
+	public void hashPassword() {
+		this.passwordUser = BCrypt.hashpw(this.passwordUser, BCrypt.gensalt());
+	}
+	
+}
