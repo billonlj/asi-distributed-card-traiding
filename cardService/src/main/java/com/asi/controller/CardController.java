92b186c189443c5117ecdb2566efa63c4ced8e8d
Giing <lazareth.baptiste@gmail.com>
2022-05-31 23:00:25 +0200

    buy a card

--- cardService/src/main/java/com/asi/controller/CardController.java
+++ cardService/src/main/java/com/asi/controller/CardController.java
@@ -3,6 +3,8 @@ package com.asi.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,7 @@ public class CardController {
 	@Autowired
 	CardService cardService;
 	
+	// Renvoie un template d'une carte
 	@GetMapping("/{id}")
 	public ResponseEntity<CardDto> get(@PathVariable int id) {
 		Card c = cardService.getCard(id);
@@ -38,6 +42,7 @@ public class CardController {
 		return new ResponseEntity<CardDto>(convertToCardDto(c), HttpStatus.OK);
 	}
 	
+	// Renvoie toutes les cartes existantes dans le jeu
 	@GetMapping("/")
 	public List<CardDto> getAll() {
 		List<Card> cards = cardService.getAll();
@@ -46,16 +51,28 @@ public class CardController {
 				.collect(Collectors.toList());
 	}
 	
-	// TODO
+	// TODO : ajout d'une carte à un utilisateur
 	@PostMapping("/users/")
 	public void add(CardInstanceDto cardInstanceDto) {
 		CardInstance cardInstance = convertToCardInstanceModel(cardInstanceDto);
 	}
+
+	//
+	@PutMapping("/users/")
+	public ResponseEntity<CardInstanceDto> buyCard(CardInstanceDto cardInstanceDto) {
+		CardInstance cardInstance = convertToCardInstanceModel(cardInstanceDto);
+		
+		CardInstance boughtCard = cardService.buyCard(cardInstance);
+		if (boughtCard == null)
+			return ResponseEntity.internalServerError().build();
+		
+		return new ResponseEntity<CardInstanceDto>(convertToCardInstanceDto(boughtCard), HttpStatus.OK);
+	}
 	
+	// Génère 5 cartes aléatoires pour un utilisateur qui s'inscrit
 	@PostMapping("/users/register/{idUser}")
 	public ResponseEntity<List<CardInstanceDto>> generateCardsForNewUser(@PathVariable int idUser) {
 		// On ignore les propriétés sources qui peuvent matcher plusieurs propriétés des champs du DTO
-		modelMapper.getConfiguration().setAmbiguityIgnored(true);
 		List<CardInstance> ci = cardService.registerNewUserCards(idUser);
 		
 		// Si la liste est vide on renvoie un code 500
@@ -70,10 +87,10 @@ public class CardController {
 		return new ResponseEntity<List<CardInstanceDto>>(cardInstanceDto, HttpStatus.OK);
 	}
 	
+	// Retourne toutes les cartes d'un utilisateur
 	@GetMapping("/users/{idUser}")
 	public ResponseEntity<List<CardInstanceDto>> getAllCardForOneUser(@PathVariable int idUser) {
 		// On ignore les propriétés sources qui peuvent matcher plusieurs propriétés des champs du DTO
-		modelMapper.getConfiguration().setAmbiguityIgnored(true);
 		List<CardInstance> cardInstances = cardService.getCardsByUser(idUser);
 		
 		if (cardInstances.isEmpty())
@@ -87,26 +104,35 @@ public class CardController {
 	}
 	
 	
+	// Convertie une Card en une CardDto
 	private CardDto convertToCardDto(Card card) {
 		CardDto cardDto = modelMapper.map(card, CardDto.class);
 		cardDto.setFamilyCard(card.getFamilyCard().getNameFamily());
 		return cardDto;
 	}
 	
+	// Convertie une CardInstance en CardInstanceDto
 	private CardInstanceDto convertToCardInstanceDto(CardInstance cardInstance) {
 		CardInstanceDto cardInstanceDto = modelMapper.map(cardInstance, CardInstanceDto.class);
 		cardInstanceDto.setCardInstance(cardInstance.getCardInstance().getIdCard());
 		return cardInstanceDto;
 	}
 	
+	// Convertie une CardDto en une Card
 	private Card convertToCardModel(CardDto cardDto) {
 		Card card = modelMapper.map(cardDto, Card.class);
 		return card;
 	}
 	
+	// Convertie une CardInstanceDto en une CardInstance
 	private CardInstance convertToCardInstanceModel(CardInstanceDto cardInstanceDto) {
 		CardInstance cardInstance = modelMapper.map(cardInstanceDto, CardInstance.class);
 		cardInstance.setCardInstance(cardService.getCard(cardInstanceDto.getCardInstance()));
 		return cardInstance;
 	}
+
+	@PostConstruct
+    private void postConstruct() {
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+    }
 }
--- cardService/src/main/java/com/asi/service/CardService.java
+++ cardService/src/main/java/com/asi/service/CardService.java
@@ -20,7 +20,7 @@ public class CardService {
 	CardRepository cardRepository;
 	
 	@Autowired
-	CardInstanceRepository cardinstanceRepository;
+	CardInstanceRepository cardInstanceRepository;
 	
 	private int numberOfCardThoGenerate = 5;
 	
@@ -32,7 +32,7 @@ public class CardService {
 	}
 	
 	public List<CardInstance> getCardsByUser(int idUser) {
-		Optional<List<CardInstance>> oCards = cardinstanceRepository.findByIdUser(idUser);
+		Optional<List<CardInstance>> oCards = cardInstanceRepository.findByIdUser(idUser);
 		if (oCards.isPresent())
 			return oCards.get();
 		return new ArrayList<CardInstance>();
@@ -63,9 +63,21 @@ public class CardService {
 		for (Card card : cards) {
 			CardInstance ci = convertCardToCardInstance(card);
 			ci.setIdUser(idUser);
-			cardsInstances.add(cardinstanceRepository.save(ci));
+			cardsInstances.add(cardInstanceRepository.save(ci));
 		}
 		return cardsInstances;
+	}
+
+	public CardInstance buyCard(CardInstance card) {
+		Optional<CardInstance> oCardToUpdate = cardInstanceRepository.findById(card.getIdInstance());
+
+		if (!oCardToUpdate.isPresent())
+			throw new RuntimeException("Card to buy don't exist");
+
+		CardInstance cardToUpdate = oCardToUpdate.get();
+
+		cardToUpdate.setIdUser(card.getIdUser());
+		return cardInstanceRepository.save(cardToUpdate);
 	}
 	
 	private CardInstance convertCardToCardInstance(Card card) {
